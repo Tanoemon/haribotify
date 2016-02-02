3fcb0266730b38d8a61c969ca674bd850c61e106
nanila <u@uio.jp>
2016-02-02 18:35:33 +0900

    use absolute paths

--- test/run.js
+++ test/run.js
@@ -4,24 +4,25 @@ var fs = require('fs');
 var browserify = require('browserify');
 var haribotify = require('../');
 var vm = require('vm');
+var path = require('path');
 
 test('retrieve', function(t) {
   t.plan(1);
 
-  var expected = fs.readFileSync('./expected.html', 'utf8');
-  var result = retrieve('./data/base.html');
+  var expected = fs.readFileSync(path.join(__dirname, 'expected.html'), 'utf8');
+  var result = retrieve(path.join(__dirname, './data/base.html'));
   t.equal(result, expected);
 });
 
 test('haribotify', function(t) {
   t.plan(1);
 
-  var b = browserify('./entry.js');
+  var b = browserify(path.join(__dirname, './entry.js'));
   b.transform(haribotify);
   b.on('transform', function (tr, file) {
     if (tr.constructor.name !== 'Haribotify') { return; }
     tr.once('haribotify', function (result) {
-      var html = fs.readFileSync('./expected.html', 'utf8');
+      var html = fs.readFileSync(path.join(__dirname, './expected.html'), 'utf8');
       var replaced = html.replace(/\r\n|\r/g, "\\n");
       var expected = "module.exports = '" + replaced + "';";
       t.equal(result, expected);
@@ -33,12 +34,12 @@ test('haribotify', function(t) {
 test('browserify', function(t) {
   t.plan(1);
 
-  var b = browserify('./entry.js');
+  var b = browserify(path.join(__dirname, './entry.js'));
   b.transform(haribotify);
   b.bundle(function(err, src) {
     var c = {};
     vm.runInNewContext(src, c);
-    var expected = fs.readFileSync('./expected.html', 'utf8');
+    var expected = fs.readFileSync(path.join(__dirname, './expected.html'), 'utf8');
     var expectedJs = expected.replace(/\r\n|\r/g, "\n");
     t.equal(c.result, expectedJs);
   });
